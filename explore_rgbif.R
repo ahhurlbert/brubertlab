30adf7e94634fb553a58da19b0b0c63901e8d937
Jes Coyle <jrcoyle@live.unc.edu>
2014-03-25 15:04:08 -0400

    added bluefin stuff to rgbif script

--- explore_rgbif.R
+++ explore_rgbif.R
@@ -19,6 +19,11 @@ library(dismo)
 pyrenula = name_lookup('Pyrenula', rank='species', limit=1000, status='accepted')
 pyrenula = name_lookup('Pyrenula', rank='species', limit=1000, status='accepted')$data
 
+
+bftuna = name_lookup('Thunnus thynnus', rank='species', limit=1000, status='accepted')$data
+
+bfsp = subset(bftuna, phylum=='Chordata')$nubKey
+
 # Keep only species in Pyrenula genus
 pyrenula = subset(pyrenula, parent=='Pyrenula')
 
@@ -32,6 +37,9 @@ pyrenula_sp = unique(pyrenula_sp); dim(pyrenula_sp)
 # Make dataframe of number of records for each species
 pyrenula_sp$numRec = sapply(pyrenula_sp$nubKey, function(x) occ_count(nubKey=x))
 
+
+bfsp 
+
 # Which species have the most records?
 pyrenula_sp = pyrenula_sp[order(pyrenula_sp$numRec, decreasing=T),]
 
@@ -44,10 +52,25 @@ pyrenula10_recs = occ_search(taxonKey=use_sp[1], spatialIssues=F, georeferenced=
 	return='data', fields='minimal', limit=500)
 names(pyrenula10_recs) # use to choose which fields wanted
 
+
+# Bluefin tuna
+bfsp = unique(bfsp)
+
+myrecs = data.frame()
+for(i in 1:length(bfsp)){
+	newdata = occ_search(taxonKey=bfsp[i],spatialIssues=F, georeferenced=T,
+		return='data', fields='minimal', limit=500)
+	myrecs = rbind(myrecs, newdata)
+}
+
+library(maps)
+map('world')
+points(myrecs$longitude, myrecs$latitude)
+
+
 library(sp)
 p_sp = pyrenula10_recs
 coordinates(p_sp) = c('longitude','latitude')
-
 gbifmap(pyrenula10_recs)
 
 # For genus Pyrenula 
